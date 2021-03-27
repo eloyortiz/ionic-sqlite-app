6c0a9f0c79f4fa5ba801c53a48cac6334bcc556a
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2021-03-27 11:51:15 +0100

    se implementa Front

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,16 +1,60 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormsModule } from '@angular/forms';
+import { DbService } from '../../services/db.service';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor( ) {
+  mainForm: FormGroup;
+  Data: any[] = [];
+  
+  
+  constructor( 
+    private dbSrv: DbService,
+    private formBuilder: FormBuilder,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) { }
 
-    
+  ngOnInit(): void {
+    this.dbSrv.dbState()
+      .subscribe( (res) => {
+        if(res){
+          this.dbSrv.fetchSongs()
+            .subscribe( item => {
+              this.Data = item;
+            })
+        }
+      });
+
+      this.mainForm = this.formBuilder.group({
+        artist: [''],
+        song: ['']
+      })
     
   }
+
+  storeData(){
+    this.dbSrv.addSong( this.mainForm.value.artist, this.mainForm.value.song )
+      .then( res => {
+        this.mainForm.reset();
+      });
+  }
+
+  deleteSong(id){
+    this.db.deleteSong(id).then(async(res) => {
+      let toast = await this.toast.create({
+        message: 'Song deleted',
+        duration: 2500
+      });
+      toast.present();      
+    })
+  }
+
 
 }
--- src/app/pages/song/song.page.ts
+++ src/app/pages/song/song.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { DbService } from '../../services/db.service';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-song',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./song.page.scss'],
 })
 export class SongPage implements OnInit {
+  editForm: FormGroup;
+  id: any;
 
-  constructor() { }
+  constructor(
+    private db: DbService,
+    private router: Router,
+    public formBuilder: FormBuilder,
+    private actRoute: ActivatedRoute
+  ) {
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+
+    this.db.getSong(this.id).then(res => {
+      this.editForm.setValue({
+        artist_name: res['artist_name'],
+        song_name: res['song_name']
+      })
+    })
+  }
 
   ngOnInit() {
+    this.editForm = this.formBuilder.group({
+      artist_name: [''],
+      song_name: ['']
+    })
+  }
+
+  saveForm(){
+    this.db.updateSong(this.id, this.editForm.value)
+    .then( (res) => {
+      console.log(res)
+      this.router.navigate(['/home']);
+    })
   }
 
 }
